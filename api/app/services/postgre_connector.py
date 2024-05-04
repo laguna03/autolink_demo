00629ee6fb906c56345d5dcc6d5907a965e1c280
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-04 19:56:51 -0400

    new branch and postgres connector

---
+++ api/app/services/postgre_connector.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+def connect_to_database():
+    try:
+        # Establish connection to the PostgreSQL database
+        conn = psycopg2.connect(
+            dbname="autolinkdb",
+            user="pedrolaguna",
+            password="autolink2024",
+            host="localhost",
+            port="5432"
+        )
+        print("Connection to the database successful")
+        return conn
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to PostgreSQL:", error)
+        return None
+
+def close_connection(conn):
+    if conn:
+        conn.close()
+        print("Connection to the database closed")
+
+def create_cursor(conn):
+    try:
+        # Create a cursor
+        cur = conn.cursor()
+        print("Cursor created successfully")
+        return cur
+    except (Exception, psycopg2.Error) as error:
+        print("Error while creating cursor:", error)
+        return None
