fb7498a2a9b125823f6d1e0be70191e62ecc27a9
MMD <manuelmoralesdiaz0@gmail.com>
2024-05-14 20:59:35 -0400

    changing read_client function code

--- api/app/models/client_operations.py
+++ api/app/models/client_operations.py
@@ -36,16 +36,46 @@ def create_client(client_data):
         conn.close()
 
 
-def read_client(client_id):
+def read_client(client_id=None, first_name=None, last_name=None, email=None, phone=None, date_of_birth=None):
+    # Create a list to store conditions for WHERE clause
+    conditions = []
+    params = []
+
+    # Check each parameter and add condition to the list if not None
+    if client_id:
+        conditions.append("id = %s")
+        params.append(str(client_id))
+    if first_name:
+        conditions.append("first_name = %s")
+        params.append(first_name)
+    if last_name:
+        conditions.append("last_name = %s")
+        params.append(last_name)
+    if email:
+        conditions.append("email = %s")
+        params.append(email)
+    if phone:
+        conditions.append("phone = %s")
+        params.append(phone)
+    if date_of_birth:
+        conditions.append("date_of_birth = %s")
+        params.append(date_of_birth)
+
+    # Join conditions with 'AND' and create the WHERE clause
+    where_clause = " AND ".join(conditions)
+
+    # Connect to the database
     conn = postgre_connector.connect_to_database()
     try:
         cur = conn.cursor()
-        query = "SELECT * FROM clients WHERE id = %s"
-        cur.execute(query, (str(client_id),))
-        client = cur.fetchone()
-        if client is None:
-            raise HTTPException(status_code=404, detail="Client not found")
-        return ClientData(
+        query = "SELECT * FROM clients"
+        if where_clause:
+            query += " WHERE " + where_clause
+        cur.execute(query, tuple(params))
+        clients = cur.fetchall()
+        if not clients:
+            raise HTTPException(status_code=404, detail="Clients not found")
+        return [ClientData(
             id=client[0],
             updated_at=client[1],
             first_name=client[2],
@@ -53,7 +83,7 @@ def read_client(client_id):
             email=client[4],
             phone=client[5],
             date_of_birth=client[6]
-        )
+        ) for client in clients]
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     finally:
