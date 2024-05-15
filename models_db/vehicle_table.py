06df640578e37d9b63001c08720fc0202a58477d
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-05-15 18:52:27 -0400

    adding table code for testing

---
+++ models_db/vehicle_table.py
@@ -0,0 +1,28 @@
+""" Create a table in the PostgreSQL database """
+
+import psycopg2
+
+
+# Establish a connection to the database by creating a connection object
+db_connection = psycopg2.connect(
+    dbname="autolinkdb",
+    user="pedrolaguna",
+    password="autolink2024",
+    host="localhost",
+    port="5432"
+)
+
+# Create a cursor object using the cursor() method
+cur = db_connection.cursor()
+
+
+#define sql command to create a table
+cur.execute("""
+ALTER TABLE vehicle
+ADD FOREIGN KEY (client_id) REFERENCES client(client_id);
+""")
+
+db_connection.commit()
+
+cur.close()
+db_connection.close()
