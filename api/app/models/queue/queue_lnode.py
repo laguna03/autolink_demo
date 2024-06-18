16f078a5052ae5907d403631e41190a114e3987b
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-06-18 14:22:45 -0400

    fixing exit and front element dequeue

--- api/app/models/queue/queue_lnode.py
+++ api/app/models/queue/queue_lnode.py
@@ -25,28 +25,31 @@ class Queue:
             self.rear.next = new_node
             self.rear = new_node
 
-    # Method to remove an item from the queue
-    def DeQueue(self, item):
-        if self.front == None:
+    # Method to remove a specific item from the queue
+    def DeQueue(self, x):
+        if self.front is None:
             print("Queue is empty")
-        elif self.front.next == item: # No deja que borre el primero en el Q
-            print("Dequeued item is: ", self.front.data)
-            print("-------------------------")
-            self.front = self.front.next
         else:
-            prev = self.front
-            temp = self.front.next
-            while temp is not None and temp.data != item:
-                prev = temp
-                temp = temp.next
-            if temp is None:
-                print("Item not found in the queue")
-            else:
-                print("Dequeued item is: ", temp.data)
+            if self.front.data == x:  # Check if the front item is the one to be dequeued
+                print("Dequeued item is: ", self.front.data)
                 print("-------------------------")
-                prev.next = temp.next
-                if temp == self.rear:
-                    self.rear = prev
+                self.front = self.front.next
+                if self.front is None:  # If the queue is now empty, update the rear pointer
+                    self.rear = None
+            else:
+                prev = self.front
+                temp = self.front.next
+                while temp is not None and temp.data != x:
+                    prev = temp
+                    temp = temp.next
+                if temp is None:
+                    print("Item not found in queue")
+                else:
+                    print("Dequeued item is: ", temp.data)
+                    print("-------------------------")
+                    prev.next = temp.next
+                    if temp == self.rear:  # If the dequeued item was the rear, update the rear pointer
+                        self.rear = prev
 
 
     # Method to display the queue
@@ -67,24 +70,27 @@ class Queue:
 # Driver code
 q = Queue()
 while 1:
-    print("Enter option: ")
-    print("1-Enqueue\n2-Dequeue\n3-Display\n4-Exit\n")
-    print("-------------------------")
-    option = int(input())
-    if option == 1:
-        print("Enqueued")
+    try:
+        print("Enter option: ")
+        print("1-Enqueue\n2-Dequeue\n3-Display\n4-Exit\n")
         print("-------------------------")
-        q.EnQueue()
-    elif option == 2:
-        item = input("Enter the element to be removed from the queue: ")
-        print("Dequeued")
-        print("-------------------------")
-        q.DeQueue (item)
-    elif option == 3:
-        print("Display")
-        q.Display()
-    elif option == 4:
-        break
-    else:
-        print("Invalid option")
-        print("-------------------------")
+        option = int(input())
+        if option == 1:
+            print("Enqueued")
+            print("-------------------------")
+            q.EnQueue()
+        elif option == 2:
+            item = input("Enter the element to be removed from the queue: ")
+            print("Dequeued")
+            print("-------------------------")
+            q.DeQueue(item)
+        elif option == 3:
+            print("Display")
+            q.Display()
+        elif option == 4:
+            break
+        else:
+            print("Invalid option")
+            print("-------------------------")
+    except Exception as e:
+        print("An error occurred: ", e)
