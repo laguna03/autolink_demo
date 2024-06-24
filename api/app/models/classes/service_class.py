aa954ae8ba2a1e3f3d72a7c4a0cae5f66f9c3e63
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-06-23 23:22:03 -0400

    service duration data type change

--- api/app/models/classes/service_class.py
+++ api/app/models/classes/service_class.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
-
+from datetime import timedelta
 
 
 class ServiceData(BaseModel):
     service_id: int
     service_name: str
     service_description: str
-    service_duration: int
+    service_duration: timedelta
     service_price: float
