5165b1f0f2e9337f7f5e5fbcb3773e26471b9c64
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-07-01 00:14:39 -0400

    updates

--- api/main.py
+++ api/main.py
@@ -45,15 +45,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
-<<<<<<< HEAD
-<<<<<<< HEAD
-    with open(os.path.join(TEMPLATE_DIR, "html/landing.html"), "r") as f:
-=======
-    with open(os.path.join(TEMPLATE_DIR, "html/create_agent.html"), "r") as f:
->>>>>>> manu
-=======
-    with open(os.path.join(TEMPLATE_DIR, "html/landing.html"), "r") as f:
->>>>>>> manu
+    with open(os.path.join(TEMPLATE_DIR, "html/landing"), "r") as f:
         return HTMLResponse(content=f.read(), status_code=200)
 
 @app.get("/login", response_class=HTMLResponse)
@@ -66,13 +58,6 @@ async def get_index():
     with open(os.path.join(TEMPLATE_DIR, "html/index.html"), "r") as f:
         return HTMLResponse(content=f.read(), status_code=200)
 
-<<<<<<< HEAD
-@app.get("/create_agent", response_class=HTMLResponse)
-async def read_root():
-    with open(os.path.join(TEMPLATE_DIR, "html/create_agent.html"), "r") as f:
-        return HTMLResponse(content=f.read(), status_code=200)
-
-=======
 @app.get("/queue", response_class=HTMLResponse)
 async def get_index():
     with open(os.path.join(TEMPLATE_DIR, "html/dashboard.html"), "r") as f:
@@ -83,25 +68,11 @@ async def get_index():
     with open(os.path.join(TEMPLATE_DIR, "html/create_client.html"), "r") as f:
         return HTMLResponse(content=f.read(), status_code=200)
 
-@app.get("/interface", response_class=HTMLResponse)
-async def get_index():
-    with open(os.path.join(TEMPLATE_DIR, "html/interface.html"), "r") as f:
-        return HTMLResponse(content=f.read(), status_code=200)
 
-@app.get("/clients", response_class=HTMLResponse)
-async def get_index():
-    with open(os.path.join(TEMPLATE_DIR, "html/clients.html"), "r") as f:
-        return HTMLResponse(content=f.read(), status_code=200)
-
->>>>>>> manu
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 add_routers(app)
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("API_PORT", "8000")))
-=======
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("API_PORT", "8080")))
->>>>>>> manu
